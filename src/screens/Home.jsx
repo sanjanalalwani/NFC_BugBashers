77c309c0a9216eb2c3bb50db80ead09c636f689c
Renee David <102434516+ReneeJascintha@users.noreply.github.com>
2023-09-15 11:59:01 +0530

    init

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -5,7 +5,7 @@ export default function Home(){
     return(
         <div className="home">
 
-1. cards of module and all
+1. cards of modules  and all - home page
 {/* 1. Module 2. Expense Tracker 3.Game 4.Community 5.Quiz */}
         </div>
     )
