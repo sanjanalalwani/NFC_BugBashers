abd09d09f9c5db5e09090c705ed6c4e16bb650c8
Renee David <102434516+ReneeJascintha@users.noreply.github.com>
2023-09-15 17:29:42 +0530

    init

--- src/screens/Module.jsx
+++ src/screens/Module.jsx
@@ -1,8 +1,44 @@
 import React from "react";
+import "../css/module.css";
+import { Link } from 'react-router-dom'; 
 
 export default function Module(){
     return(
         <div>
-        module</div>
+         <div className="mod" style={{width:"300px"}}>
+            <Link to="/module" className="nav-link" id="tracker">
+                <div className="card">
+                    <h2>1. Understanding Income and Expenses</h2>
+                    <hr></hr>
+                </div>
+                </Link>
+                <Link to="/community" className="nav-link" id="tracker">
+                <div className="card">
+                    <h2>COMMUNITY</h2>
+                    <hr></hr>
+                </div>
+                </Link>
+                <Link to="/game" className="nav-link" id="tracker">
+                <div className="card">
+                <h2>GAME</h2>
+                    <hr></hr>
+                    
+                </div>
+                </Link>
+                <Link to="/expense" className="nav-link" id="tracker">
+                <div className="card">
+                <h2>EXPENSE TRACKER</h2>
+                    <hr></hr>
+                
+                </div>
+              </Link>
+              <Link to="/quiz" className="nav-link" id="tracker">
+                <div className="card">
+                <h2>QUIZ</h2>
+                    <hr></hr>
+                   
+                </div>
+                </Link>
+            </div></div>
     )
 }
