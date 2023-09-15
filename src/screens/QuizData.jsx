de24eb00702c947c71102d3c28ba091081f80731
sanjanalalwani <610sanjana@gmail.com>
2023-09-16 00:34:32 +0530

    init

--- src/screens/QuizData.jsx
+++ src/screens/QuizData.jsx
@@ -1,22 +1,22 @@
 export const QuizData = [
     {
-        question: "Which language runs in a web browser?",
-        options:[ "Java", "C", "Python", "JavaScript"],
-        answer: 4
-    },
-    {
-        question: "What does CSS stand for?",
-        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        question: "What is the definition of 'earned income'?",
+        options:[ "Income generated from investments", "Compensation received for active participation in a job or business", "Passive income"],
         answer: 2
     },
     {
-        question: "What does HTML stand for?",
-        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        question: "Name one practical tool or technique for tracking expenses:",
+        options:["Budgeting app", "Spreadsheet tracking", " Both a and b"],
+        answer: 3
+    },
+    {
+        question: "Why is it important to establish clear financial goals before creating a budget?",
+        options: ["It helps in prioritizing expenses","It allows for more discretionary spending","It's not necessary to have clear goals before budgeting"],
         answer: 1
     },
     {
-        question: "What year was JavaScript launched?",
-        options: ["1996", "1995", "1994", "none of the above"],
+        question: "What is the purpose of an emergency fund?",
+        options: ["To fund vacations and entertainment", "To provide a financial buffer for unexpected expenses", " To invest in the stock market"],
         answer:2
     },
 ];
