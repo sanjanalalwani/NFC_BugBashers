ee027e65e4a3182d46884e3bca1529d4e05a0052
Renee David <102434516+ReneeJascintha@users.noreply.github.com>
2023-09-15 23:04:54 +0530

    init

--- src/screens/Module1.jsx
+++ src/screens/Module1.jsx
@@ -5,7 +5,11 @@ import { Link } from 'react-router-dom';
 export default function Module1(){
     return (
         <div>
-        hello
+        <div className="title"><span>Module 1:</span> <p>Understanding Income and Expenses</p></div>
+        <hr className="line"></hr>
+        <div className="box">
+       <iframe src="https://www.youtube.com/embed/eAUIM4rFEVg?si=A8bJ_NDRjJvEmw9n" frameborder="0" ></iframe></div>
+        
         </div>
     )
 }
