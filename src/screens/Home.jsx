13e18a592ea509e76e742b49b61bf3d278ce19b8
Renee David <102434516+ReneeJascintha@users.noreply.github.com>
2023-09-15 13:33:08 +0530

    init

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -4,9 +4,12 @@ import "../css/home.css"
 export default function Home(){
     return(
         <div className="home">
-
+            <div className="content"><h1>Welcome to <span className="wealth">संपत्ति,</span></h1>
+            <p>where we empower individuals with essential financial knowledge. Our mission is to equip individuals and families with the tools they need to achieve lasting financial stability and independence.</p></div>
+            <img src="home page image.jpg" alt="" className="doodle" />
+            
 {/*1. cards of modules  and all - home page
- 1. Module 2. Expense Tracker 3.Game 4.Community 5.Quiz */}
+ 1. Modules 2. Expense Tracker 3.Game 4.Community 5.Quiz */}
         </div>
     )
 }
